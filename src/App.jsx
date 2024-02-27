45a0a15fe3322f9376856512cd25c2ac4adea607
zahraa <zahraaalihasan279@gmail.com>
2024-02-27 15:44:03 +0300

    login

--- src/App.jsx
+++ src/App.jsx
@@ -5,18 +5,27 @@ import Nav from './components/Nav'
 import About from './pages/About'
 import AddGame from './pages/AddGame'
 import GameDetails from './pages/GameDetails'
-const App = () => (
-  <div>
-    <Nav />
-    <main>
-      <Routes>
-        <Route path="/game" element={<Home />} />
-        <Route path="/game/:id" element={<GameDetails />} />
-        <Route path="/about" element={<About />} />
-        <Route path="/addgames" element={<AddGame />} />
-      </Routes>
-    </main>
-  </div>
-)
+import Register from './pages/Register'
+import SignIn from './components/SingnIn'
+import { useEffect, useRef, useState } from 'react'
+
+const App = () => {
+  const [user, setUser] = useState(null)
+  return (
+    <div>
+      <Nav />
+      <main>
+        <Routes>
+          <Route path="/signin" element={<SignIn setUser={setUser} />} />
+          <Route path="/register" element={<Register />} />
+          <Route path="/game" element={<Home />} />
+          <Route path="/game/:id" element={<GameDetails />} />
+          <Route path="/about" element={<About />} />
+          <Route path="/addgames" element={<AddGame />} />
+        </Routes>
+      </main>
+    </div>
+  )
+}
 
 export default App
