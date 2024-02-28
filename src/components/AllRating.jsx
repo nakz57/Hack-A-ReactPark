71474d1523b6e33533bb0383ef5915098c926c47
aboodabdo347 <aboodabdo347@gmail.com>
2024-02-28 09:01:47 +0300

    style the gamedetails page

--- src/components/AllRating.jsx
+++ src/components/AllRating.jsx
@@ -1,12 +1,24 @@
 const AllRating = ({ ratings }) => {
+  let avarageRating = 0
+  if (ratings) {
+    const allrating = ratings.map((rating) => rating.rating)
+    console.log('allrating', allrating)
+    const sum = allrating.reduce((accumulator, val) => {
+      return accumulator + val
+    }, 0)
+    avarageRating = Math.round(sum / allrating.length)
+  }
   return (
-    <div>
-      <h3>Reviews: </h3>
+    <div className="all-rating-container">
+      {avarageRating ? (
+        <h3>Reviews: {Array(avarageRating).fill(<span className="star"></span>)}</h3>
+      ) : null}
       {ratings ? (
         ratings.map((rating) => (
-          <div key={rating._id}>
-            <h4>{rating.rating}</h4>
-            <h3>{rating.content}</h3>
+          <div className="rating-box" key={rating._id}>
+            <h4> {Array(rating.rating).fill(<span className="star"></span>)}</h4>
+            <h3> Contant: {rating.content}</h3>
+
             {/* <img src={review.pic} alt={review.title} /> */}
           </div>
         ))
