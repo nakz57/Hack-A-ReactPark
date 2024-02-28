47ae8f018efb00acb682788443e6aeaf9a38337a
Nayef Alzayani <nzayani57@outlook.com>
2024-02-28 09:06:53 +0300

    part of the CSS

--- src/components/AllRating.jsx
+++ src/components/AllRating.jsx
@@ -1,7 +1,18 @@
 const AllRating = ({ ratings }) => {
+  let averageRating = 0
+  if (ratings) {
+    const allrating = ratings.map((rating) => rating.rating)
+    console.log('allrating', allrating)
+    const sum = allrating.reduce((accumulator, val) => {
+      return accumulator + val
+    }, 0)
+    averageRating = Math.round(sum / allrating.length)
+  }
+
   return (
     <div>
       <h3>Reviews: </h3>
+      <h3>Reviews: {Array(averageRating).fill(<span>:star:</span>)}</h3>
       {ratings ? (
         ratings.map((rating) => (
           <div key={rating._id}>
--- src/components/Nav.jsx
+++ src/components/Nav.jsx
@@ -4,22 +4,37 @@ const Nav = ({ user, handleLogOut }) => {
   let userOptions
   if (user) {
     userOptions = (
-      <nav>
-        <h3>Welcome {user.email}!</h3>
-        <Link onClick={handleLogOut} to="/game">
-          Sign Out
+      <nav className="nav-bar">
+        <h3>Welcome! {user.email}</h3>
+        <Link className="Link" to="/game">
+          Home
         </Link>
         <Link className="Link" to="/addgames">
           Add Games
+        </Link>
+        <Link className="Link" to="about">
+          About
+        </Link>
+        <Link className="Link" onClick={handleLogOut} to="/game">
+          Sign Out
         </Link>
       </nav>
     )
   }
   const publicOptions = (
-    <nav>
-      <Link to="/game">Home</Link>
-      <Link to="/register">Register</Link>
-      <Link to="/signin">Sign In</Link>
+    <nav className="nav-bar">
+      <Link className="Link" to="/game">
+        Home
+      </Link>
+      <Link className="Link" to="/register">
+        Register
+      </Link>
+      <Link className="Link" to="about">
+        About
+      </Link>
+      <Link className="Link" to="/signin">
+        Sign In
+      </Link>
     </nav>
   )
   return (
